29b63cfa9a0674779502c7f582e09c5678dd1279
aditya290702 <146001800+aditya290702@users.noreply.github.com>
2023-11-06 12:59:43 +0530

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+Quizdf = pd.read_csv("Quiz.csv", delimiter=",", usecols=['Question', 'QuestionNo'])
+Answer = pd.read_csv("Quiz.csv", delimiter=",", usecols=['AnswerOption'])
+#print(Quizdf)
+print("Enter your name")
+Name = input()
+print("HI",Name,"!!!! You can attempt this quiz only 2 times")
+print("Read the Questions carefully & Select Your Option....")
+print("You will get 1mark for each correct answer and 0 for Wrong")
+
+
+
+def PythonQuiz():
+    count = 0
+    total = 10
+    for i in range(1, 11):
+        Question = (Quizdf['Question'][i])
+        Solution = (Answer['AnswerOption'][i])
+        print(Question)
+        OptionInput = input()
+        if(OptionInput == Solution):
+            print("correct")
+            count+=1
+        else:
+            print("wrong","the correct option was",Solution)
+
+    print("Dear",Name,"THANKYOU FOR ATTEMPTING THE QUIZ")
+    print("your total score is",count,"out of",total)
+
+def main():
+    #OptionInput = input()
+    x = PythonQuiz()
+    print(x)
+
+    print("Wanna Attempt Again????  TYPE Y or N")
+    Again = input()
+    if (Again == "Y"):
+        x = PythonQuiz()
+        print(x)
+    else:
+        if(Again == "N"):
+            print("Thankyou : )")
+    #try:
+    #    print(Again)
+    #except:
+    #    print('Need an Input in either Y or N but got', Again)
+
+    #    print("continue")
+
+if __name__=="__main__":
+    main()
