9467de099958914a5a0ba26bea8fc3e8cfe12b15
aditya290702 <146001800+aditya290702@users.noreply.github.com>
2024-02-07 10:59:55 +0530

    Update and rename Batch Gradient Descent 1.py to MACHINE_LEARNING_LAB3.py

--- MACHINE_LEARNING_LAB3.py
+++ MACHINE_LEARNING_LAB3.py
@@ -1,3 +1,22 @@
+# Class exercise
+
+# Write hypothesis function for d features
+# Write cost function for n samples and d features
+# Write derivative of a cost function for one sample and one feature
+# Write parameter update rule for one parameter and one sample
+# Write parameter update rule for d parameters and one sample
+# Write parameter update rule for d parameters and n samples
+
+# Implement batch gradient descent
+
+# Read simulated data csv file
+# Form x and y
+# Write a function to compute hypothesis
+# Write a function to compute the cost
+# Write a function to compute the derivative
+# Write update parameters logic in the main function
+
+
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
